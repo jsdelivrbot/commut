ba63a8687587ef6158510e5937c171f1abe012cc
Matt Wilkin <mwilkin22@gmail.com>
2016-11-02 14:21:45 -0700

    add api call result data add rough framework to make an api call

---
+++ app/api/tsa_precheck.jsx
@@ -0,0 +1,159 @@
+[
+  {
+    "airport": {
+      "name": "Los Angeles International",
+      "shortcode": "LAX",
+      "city": "Los Angeles",
+      "state": "CA",
+      "latitude": 33.942452,
+      "longitude": -118.40801,
+      "utc": -8,
+      "dst": true,
+      "precheck": true,
+      "precheckMilitary": false,
+      "checkpoints": [
+        {
+          "id": 375,
+          "longname": "TB International - North",
+          "shortname": "TBIT- NORTH",
+          "precheck": false,
+          "precheckMilitary": false,
+          "precheckCarriers": []
+        },
+        {
+          "id": 381,
+          "longname": "TB International - South",
+          "shortname": "TBIT- SOUTH",
+          "precheck": false,
+          "precheckMilitary": false,
+          "precheckCarriers": []
+        },
+        {
+          "id": 382,
+          "longname": "Terminal 1",
+          "shortname": "Terminal 1",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "Aeromexico"
+            },
+            {
+              "name": "Allegiant Air"
+            },
+            {
+              "name": "LUFTHANSA"
+            },
+            {
+              "name": "Etihad Airways"
+            },
+            {
+              "name": "FRONTIER AIRLINES, INC."
+            },
+            {
+              "name": "Southwest Airlines"
+            },
+            {
+              "name": "WestJet"
+            }
+          ]
+        },
+        {
+          "id": 380,
+          "longname": "Terminal 2",
+          "shortname": "Terminal 2",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "Air Canada"
+            },
+            {
+              "name": "Sun Country Airlines"
+            }
+          ]
+        },
+        {
+          "id": 379,
+          "longname": "Terminal 3",
+          "shortname": "Terminal 3",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "JetBlue Airways"
+            },
+            {
+              "name": "Virgin America"
+            }
+          ]
+        },
+        {
+          "id": 376,
+          "longname": "Terminal 4",
+          "shortname": "Terminal 4",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "American Airlines"
+            }
+          ]
+        },
+        {
+          "id": 383,
+          "longname": "Terminal 5",
+          "shortname": "Terminal 5",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "Delta Air Lines"
+            }
+          ]
+        },
+        {
+          "id": 384,
+          "longname": "Terminal 6",
+          "shortname": "Terminal 6",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "Alaska Airlines"
+            },
+            {
+              "name": "United Airlines"
+            }
+          ]
+        },
+        {
+          "id": 378,
+          "longname": "Terminal 7",
+          "shortname": "Terminal 7",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "United Airlines"
+            }
+          ]
+        },
+        {
+          "id": 377,
+          "longname": "Terminal 8",
+          "shortname": "Terminal 8",
+          "precheck": true,
+          "precheckMilitary": false,
+          "precheckCarriers": [
+            {
+              "name": "United Airlines"
+            }
+          ]
+        }
+      ],
+      "precheckNotes": [
+        {
+          "Note": "For more information please visit https://www.tsa.gov/precheck."
+        }
+      ]
---
+++ app/api/tsa_wait_time.jsx
@@ -0,0 +1,129 @@
+{
+  "WaitTimes": [
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "2",
+      "Created_Datetime": "11/2/2016 12:32:02 PM"
+    },
+    {
+      "CheckpointIndex": "1",
+      "WaitTime": "1",
+      "Created_Datetime": "11/2/2016 10:30:43 AM"
+    },
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "2",
+      "Created_Datetime": "11/2/2016 10:04:41 AM"
+    },
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "4",
+      "Created_Datetime": "11/2/2016 4:25:52 AM"
+    },
+    {
+      "CheckpointIndex": "7",
+      "WaitTime": "3",
+      "Created_Datetime": "11/1/2016 4:16:00 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 3:12:13 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 2:31:06 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 2:31:06 PM"
+    },
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 1:29:16 PM"
+    },
+    {
+      "CheckpointIndex": "10",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 1:28:48 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 12:20:50 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "11/1/2016 12:20:21 PM"
+    },
+    {
+      "CheckpointIndex": "10",
+      "WaitTime": "1",
+      "Created_Datetime": "10/31/2016 10:10:28 PM"
+    },
+    {
+      "CheckpointIndex": "4",
+      "WaitTime": "3",
+      "Created_Datetime": "10/31/2016 7:24:10 PM"
+    },
+    {
+      "CheckpointIndex": "4",
+      "WaitTime": "1",
+      "Created_Datetime": "10/31/2016 7:21:20 PM"
+    },
+    {
+      "CheckpointIndex": "4",
+      "WaitTime": "4",
+      "Created_Datetime": "10/31/2016 7:12:53 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "3",
+      "Created_Datetime": "10/31/2016 6:17:07 PM"
+    },
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "3",
+      "Created_Datetime": "10/31/2016 3:01:49 PM"
+    },
+    {
+      "CheckpointIndex": "1",
+      "WaitTime": "6",
+      "Created_Datetime": "10/31/2016 12:07:38 PM"
+    },
+    {
+      "CheckpointIndex": "9",
+      "WaitTime": "1",
+      "Created_Datetime": "10/31/2016 10:23:05 AM"
+    },
+    {
+      "CheckpointIndex": "10",
+      "WaitTime": "3",
+      "Created_Datetime": "10/31/2016 2:00:39 AM"
+    },
+    {
+      "CheckpointIndex": "8",
+      "WaitTime": "7",
+      "Created_Datetime": "10/30/2016 11:40:01 PM"
+    },
+    {
+      "CheckpointIndex": "0",
+      "WaitTime": "1",
+      "Created_Datetime": "10/30/2016 10:59:42 PM"
+    },
+    {
+      "CheckpointIndex": "5",
+      "WaitTime": "4",
+      "Created_Datetime": "10/30/2016 8:09:17 PM"
+    },
+    {
+      "CheckpointIndex": "5",
+      "WaitTime": "1",
+      "Created_Datetime": "10/30/2016 11:45:37 AM"
+    }
+  ]
+}
