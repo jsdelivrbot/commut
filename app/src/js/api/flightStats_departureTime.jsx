b09b474405450723de42d2855ab72dbecbee8e73
Sam Park <sampark18@gmail.com>
2016-11-17 10:51:49 -0800

    WORKS WITH DEPARTURETIME

--- app/src/js/api/flightStats_departureTime.jsx
+++ app/src/js/api/flightStats_departureTime.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
-const FLIGHT_URL = 'https://commut-api.herokuapp.com/departureTime/'
+const FLIGHT_URL = 'https://commut-api.herokuapp.com/departureTime/?'
 
 module.exports = {
   getDepartureTime: function (carrierCode, flightNumber) {
     var encodedCarrierCode = encodeURIComponent(carrierCode);
     var encodedFlightNumber = encodeURIComponent(flightNumber);
-    var requestUrl = `${FLIGHT_URL}carrierCode=${encodedCarrierCode}/flightNumber=${encodedFlightNumber}`;
+
+    var requestUrl = `${FLIGHT_URL}carrierCode=${encodedCarrierCode}&flightNumber=${encodedFlightNumber}`;
 
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
--- public/bundle.js
+++ public/bundle.js
@@ -27585,13 +27585,14 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	var FLIGHT_URL = 'https://commut-api.herokuapp.com/departureTime/';
+	var FLIGHT_URL = 'https://commut-api.herokuapp.com/departureTime/?';
 
 	module.exports = {
 	  getDepartureTime: function getDepartureTime(carrierCode, flightNumber) {
 	    var encodedCarrierCode = encodeURIComponent(carrierCode);
 	    var encodedFlightNumber = encodeURIComponent(flightNumber);
-	    var requestUrl = FLIGHT_URL + 'carrierCode=' + encodedCarrierCode + '/flightNumber=' + encodedFlightNumber;
+
+	    var requestUrl = FLIGHT_URL + 'carrierCode=' + encodedCarrierCode + '&flightNumber=' + encodedFlightNumber;
 
 	    return _axios2.default.get(requestUrl).then(function (res) {
 	      if (res.data.cod && res.data.message) {
